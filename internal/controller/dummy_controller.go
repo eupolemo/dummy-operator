da808fd72d5b2af8749e521e7b5dcdf60964d7ff
eupolemo <eupolemo@gmail.com>
2023-06-27 16:44:07 -0300

    update podStatus every 5 secs.

--- internal/controller/dummy_controller.go
+++ internal/controller/dummy_controller.go
@@ -20,12 +20,14 @@ import (
 	"context"
 	"fmt"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/types"
 	"k8s.io/client-go/tools/record"
 	"os"
 	"strings"
+	"time"
 
-	appsv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/runtime"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
@@ -45,8 +47,7 @@ type DummyReconciler struct {
 //+kubebuilder:rbac:groups=dummy.interview.com,resources=dummies/status,verbs=get;update;patch
 //+kubebuilder:rbac:groups=dummy.interview.com,resources=dummies/finalizers,verbs=update
 //+kubebuilder:rbac:groups=core,resources=events,verbs=create;patch
-//+kubebuilder:rbac:groups=apps,resources=deployments,verbs=get;list;watch;create;update;patch;delete
-//+kubebuilder:rbac:groups=core,resources=pods,verbs=get;list;watch
+//+kubebuilder:rbac:groups=core,resources=pods,verbs=get;list;watch;create;update;patch;delete
 
 // Reconcile is part of the main kubernetes reconciliation loop which aims to
 // move the current state of the cluster closer to the desired state.
@@ -61,21 +62,68 @@ func (r *DummyReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl
 	logger := log.FromContext(ctx)
 
 	dummy := &dummyv1alpha1.Dummy{}
-	r.Get(ctx, req.NamespacedName, dummy)
+	err := r.Get(ctx, req.NamespacedName, dummy)
 
 	logger.Info(dummy.Spec.Message)
 
 	//dummyDeployment := &appsv1.Deployment{}
 
-	dummy.Status.SpecEcho = dummy.Spec.Message
-
-	//dummyDeployment.Spec.Selector.MatchLabels["specEcho"] = dummy.Spec.Message
-	//if err = r.Status().Update(ctx, dummyDeployment); err != nil {
-	//	logger.Error(err, "Failed to update Dummy SpecEcho")
-	//	return ctrl.Result{}, err
-	//}
+	if dummy.Spec.Message != dummy.Status.SpecEcho {
+		dummy.Status.SpecEcho = dummy.Spec.Message
+	}
 
 	// TODO(user): your logic here
+
+	found := &corev1.Pod{}
+	err = r.Get(ctx, types.NamespacedName{Name: dummy.Name, Namespace: dummy.Namespace}, found)
+	if err != nil && apierrors.IsNotFound(err) {
+		pod, err := r.podForDummy(dummy)
+		if err != nil {
+			logger.Error(err, "Failed do define new Pod resource for Dummy")
+
+			dummy.Status.PodStatus = "Pending"
+
+			if err = r.Status().Update(ctx, dummy); err != nil {
+				logger.Error(err, "Failed to update Dummy status")
+				return ctrl.Result{}, err
+			}
+
+			return ctrl.Result{}, err
+		}
+
+		logger.Info("Creating a new Pod",
+			"Pod.Namespace", pod.Namespace, "Pod.Name", pod.Name)
+		if err = r.Create(ctx, pod); err != nil {
+			logger.Error(err, "Failed to create new Pod",
+				"Pod.Namespace", pod.Namespace, "Pod.Name", pod.Name)
+			return ctrl.Result{}, err
+		}
+
+		dummy.Status.PodStatus = "Pending"
+
+		if err = r.Status().Update(ctx, dummy); err != nil {
+			logger.Error(err, "Failed to update Dummy status")
+			return ctrl.Result{}, err
+		}
+
+		return ctrl.Result{RequeueAfter: time.Second * 5}, nil
+	} else if err != nil {
+		logger.Error(err, "Failed to get Pod")
+		return ctrl.Result{}, err
+	}
+
+	if found.Status.Phase == corev1.PodRunning {
+		dummy.Status.PodStatus = "Running"
+		logger.Info("Pod is Running!")
+	} else {
+		dummy.Status.PodStatus = "Pending"
+		logger.Info("Pod is Pending!")
+	}
+
+	if err = r.Status().Update(ctx, dummy); err != nil {
+		logger.Error(err, "Failed to update Dummy status")
+		return ctrl.Result{}, err
+	}
 
 	return ctrl.Result{}, nil
 }
@@ -88,66 +136,55 @@ func (r *DummyReconciler) doFinalizerOperationsForDummy(cr *dummyv1alpha1.Dummy)
 
 }
 
-func (r *DummyReconciler) deploymentForDummy(
-	dummy *dummyv1alpha1.Dummy) (*appsv1.Deployment, error) {
+func (r *DummyReconciler) podForDummy(
+	dummy *dummyv1alpha1.Dummy) (*corev1.Pod, error) {
 	ls := labelsForDummy(dummy.Name)
-	replicas := int32(1)
 
 	image, err := imageForDummy()
 	if err != nil {
 		return nil, err
 	}
 
-	dep := &appsv1.Deployment{
+	pod := &corev1.Pod{
 		ObjectMeta: metav1.ObjectMeta{
+			Labels:    ls,
 			Name:      dummy.Name,
 			Namespace: dummy.Namespace,
 		},
-		Spec: appsv1.DeploymentSpec{
-			Replicas: &replicas,
-			Selector: &metav1.LabelSelector{
-				MatchLabels: ls,
-			},
-			Template: corev1.PodTemplateSpec{
-				ObjectMeta: metav1.ObjectMeta{
-					Labels: ls,
+		Spec: corev1.PodSpec{
+			SecurityContext: &corev1.PodSecurityContext{
+				RunAsNonRoot: &[]bool{true}[0],
+				SeccompProfile: &corev1.SeccompProfile{
+					Type: corev1.SeccompProfileTypeRuntimeDefault,
 				},
-				Spec: corev1.PodSpec{
-					SecurityContext: &corev1.PodSecurityContext{
-						RunAsNonRoot: &[]bool{true}[0],
-						SeccompProfile: &corev1.SeccompProfile{
-							Type: corev1.SeccompProfileTypeRuntimeDefault,
+			},
+			Containers: []corev1.Container{{
+				Image:           image,
+				Name:            "dummy",
+				ImagePullPolicy: corev1.PullIfNotPresent,
+				SecurityContext: &corev1.SecurityContext{
+					RunAsNonRoot:             &[]bool{true}[0],
+					RunAsUser:                &[]int64{1001}[0],
+					AllowPrivilegeEscalation: &[]bool{false}[0],
+					Capabilities: &corev1.Capabilities{
+						Drop: []corev1.Capability{
+							"ALL",
 						},
 					},
-					Containers: []corev1.Container{{
-						Image:           image,
-						Name:            "dummy",
-						ImagePullPolicy: corev1.PullIfNotPresent,
-						SecurityContext: &corev1.SecurityContext{
-							RunAsNonRoot:             &[]bool{true}[0],
-							RunAsUser:                &[]int64{1001}[0],
-							AllowPrivilegeEscalation: &[]bool{false}[0],
-							Capabilities: &corev1.Capabilities{
-								Drop: []corev1.Capability{
-									"ALL",
-								},
-							},
-						},
-						Ports: []corev1.ContainerPort{{
-							ContainerPort: 80,
-							Name:          "dummy",
-						}},
-						Command: []string{"nginx", "-g", "daemon off;"},
-					}},
 				},
-			},
+				Ports: []corev1.ContainerPort{{
+					ContainerPort: 80,
+					Name:          "dummy",
+				}},
+				Command: []string{"nginx", "-g", "daemon off;"},
+			}},
 		},
 	}
 
-	if err := ctrl.SetControllerReference(dummy, dep, r.Scheme); err != nil {
+	if err := ctrl.SetControllerReference(dummy, pod, r.Scheme); err != nil {
 		return nil, err
 	}
-	return dep, nil
+	return pod, nil
 }
 
 func labelsForDummy(name string) map[string]string {
@@ -168,7 +205,7 @@ func imageForDummy() (string, error) {
 	var imageEnvVar = "DUMMY_IMAGE"
 	image, found := os.LookupEnv(imageEnvVar)
 	if !found {
-		return "nginx:stable-alpine", nil
+		return "nginxinc/nginx-unprivileged:latest", nil
 		//return "", fmt.Errorf("unable to find %s environment variable with the image", imageEnvVar)
 	}
 	return image, nil
@@ -178,6 +215,6 @@ func imageForDummy() (string, error) {
 func (r *DummyReconciler) SetupWithManager(mgr ctrl.Manager) error {
 	return ctrl.NewControllerManagedBy(mgr).
 		For(&dummyv1alpha1.Dummy{}).
-		Owns(&appsv1.Deployment{}).
+		Owns(&corev1.Pod{}).
 		Complete(r)
 }
